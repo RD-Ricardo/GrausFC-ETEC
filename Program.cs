41cd377e064b5091424d68ec0c116bee1726ca7a
RD-Ricardo <ricardodias169@gmail.com>
2021-03-19 20:13:05 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double num1, resp;
+
+            Console.WriteLine("GrausFC");
+            Console.WriteLine("-------");
+            Console.Write("Digite um temperatura:");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            resp =( num1 - 32  ) /  1.8;
+            Console.WriteLine($"°F = {num1}");
+            Console.WriteLine($"{num1}°F equivalem a {resp.ToString("f")}°F");
+            
+
+        }
+    }
+}
